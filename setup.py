3f24b6eb8517a72cc48857e67e558da007a3855b
Michail Semoglou <michailsemoglou@192.168.2.6>
2025-11-24 13:47:39 +0200

    Bump version to 3.2.2 - notebook bug fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="renoir-wikiart",
-    version="3.2.1",
+    version="3.2.2",
     packages=find_packages(exclude=['tests*', 'examples*', 'Research*', 'test_env*', 'Pierre-Auguste Renoir*', 'jose*']),
     python_requires=">=3.8",
     install_requires=[
